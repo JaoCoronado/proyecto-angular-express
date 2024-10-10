1581e21155155dcc45410fc8e460abcaf46a60ff
Jair Coronado <jairyesidk@gmail.com>
2024-10-09 19:07:13 -0500

    funtion editUser

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -79,13 +79,19 @@ export class InicioComponent implements OnInit, AfterViewInit {
   }
 
   infoModal(element: UserResModel) {
+    const createdAtFormatted = new Date(element.createdAt).toLocaleString('es-ES', {
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long',
+      day: 'numeric',
+    });
     Swal.fire({
       title: 'Información del Usuario',
       html: `
         <span class="badge bg-primary">ID:</span> <p class="fw-normal">${element._id}</p>
         <span class="badge bg-primary">Name:</span> <p class="fw-normal">${element.name}</p>
         <span class="badge bg-primary">Document Number:</span> <p class="fw-normal">${element.documentNumber}</p>
-        <span class="badge bg-primary">Created At:</span> <p class="fw-normal">${element.createdAt}</p>
+        <span class="badge bg-primary">Created At:</span> <p class="fw-normal">${createdAtFormatted}</p>
         <span class="badge bg-primary">Role:</span> <p class="fw-normal">${element.role}</p>
       `,
       icon: 'success',
@@ -93,6 +99,66 @@ export class InicioComponent implements OnInit, AfterViewInit {
       confirmButtonText: 'OK',
     });
   }
+  editModal(element: UserResModel) {
+    const createdAtFormatted = new Date(element.createdAt).toLocaleString('es-ES', {
+      weekday: 'long', 
+      year: 'numeric', 
+      month: 'long',
+      day: 'numeric', 
+    });
+    Swal.fire({
+      title: 'Editar Información del Usuario',
+      html: `
+        <form id="editUserForm" >
+          <div class="form-group mb-3">
+            <label for="userId" class="badge bg-primary">ID:</label>
+            <input type="text" id="userId" class="form-control form-control-sm" value="${element._id}" readonly />
+          </div>
+          <div class="form-group mb-3">
+            <label for="userName" class="badge bg-primary">Name:</label>
+            <input type="text" id="userName" class="form-control form-control-sm" value="${element.name}" />
+          </div>
+          <div class="form-group mb-3">
+            <label for="documentNumber" class="badge bg-primary">Document Number:</label>
+            <input type="text" id="documentNumber" class="form-control form-control-sm" value="${element.documentNumber}" />
+          </div>
+          <div class="form-group mb-3">
+            <label for="createdAt" class="badge bg-primary">Created At:</label>
+            <input type="text" id="createdAt" class="form-control form-control-sm" value="${createdAtFormatted}" readonly />
+          </div>
+          <div class="form-group mb-3">
+            <label for="role" class="badge bg-primary">Role:</label>
+            <input type="text" id="role" class="form-control form-control-sm" value="${element.role}" />
+          </div>
+        </form>
+      `,
+      icon: 'info',
+      showCancelButton: true,
+      confirmButtonColor: '#22bb33',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Guardar Cambios',
+      preConfirm: () => {
+        const userId = (document.getElementById('userId') as HTMLInputElement).value;
+        const userName = (document.getElementById('userName') as HTMLInputElement).value;
+        const documentNumber = (document.getElementById('documentNumber') as HTMLInputElement).value;
+        const role = (document.getElementById('role') as HTMLInputElement).value;
+
+        return {
+          _id: userId,
+          name: userName,
+          documentNumber: documentNumber,
+          role: role
+        };
+      }
+    }).then((result) => {
+      if (result.isConfirmed) {
+        
+        console.log("Datos actualizados:", result.value);
+      }
+    });
+  }
+
+
   
   
 }
