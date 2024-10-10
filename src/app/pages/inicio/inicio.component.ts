d42fc2e0dbba422caa48d894145ce23225f0ac2c
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2024-10-09 19:09:08 -0500

    Delete User

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -3,6 +3,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   inject,
+  OnDestroy,
   OnInit,
   ViewChild,
 } from '@angular/core';
@@ -39,11 +40,9 @@ import Swal from 'sweetalert2';
   styleUrl: './inicio.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InicioComponent implements OnInit, AfterViewInit {
-  storeServices = inject(StoreService);
-  private _liveAnnouncer = inject(LiveAnnouncer);
+export class InicioComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  users: UserResModel[] = [];
+  @ViewChild(MatSort) sort: MatSort;
 
   displayedColumns: string[] = [
     '_id',
@@ -55,19 +54,24 @@ export class InicioComponent implements OnInit, AfterViewInit {
   ];
   dataSource: any = '';
 
-  constructor(private userService: UserService) {}
+  users: UserResModel[] = [];
 
-  @ViewChild(MatSort) sort: MatSort;
+  userSubscription: Subscription
+
+  private _liveAnnouncer = inject(LiveAnnouncer);
+  private userService = inject(UserService)
+
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe((data: IRespUser) => {
-      this.users = data.users;
-      this.dataSource = new MatTableDataSource(this.users);
-    });
+    this.loadUser();
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription?.unsubscribe()
   }
 
   announceSortChange(sortState: Sort) {
@@ -76,6 +80,13 @@ export class InicioComponent implements OnInit, AfterViewInit {
     } else {
       this._liveAnnouncer.announce('Sorting cleared');
     }
+  }
+
+  loadUser() {
+   this.userSubscription = this.userService.getUsers().subscribe((data: IRespUser) => {
+      this.users = data.users;
+      this.dataSource = new MatTableDataSource(this.users);
+    });
   }
 
   infoModal(element: UserResModel) {
@@ -93,6 +104,25 @@ export class InicioComponent implements OnInit, AfterViewInit {
       confirmButtonText: 'OK',
     });
   }
-  
-  
+
+  deleteUser(user: UserResModel) {
+    Swal.fire({
+      title: 'Desea eliminar este usuario',
+      icon: 'warning',
+      html: `¿Seguro de eliminar el usuario ${user.name}?`,
+      showCancelButton: true,
+      focusConfirm: true,
+      confirmButtonText: `Sí`,
+      cancelButtonText: `No`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.userService.deleteUser(user._id).subscribe((resp: any) => {
+          this.loadUser();
+          Swal.fire('Usuario Eliminado!', `${resp.msg}`, 'success');
+        });
+      } else if (result.isDismissed) {
+        Swal.fire('No lo elimina', '', 'info');
+      }
+    });
+  }
 }
